5b8dee4b073f25c88183a7d047525d9fa67c8020
Josh Hale <hale.j.0810@gmail.com>
2016-06-14 13:00:13 +0100

    leaderboard basic structure

--- app/leaderboard/leaderboard.js
+++ app/leaderboard/leaderboard.js
@@ -9,6 +9,35 @@ angular.module('myApp.leaderboard', ['ngRoute'])
   });
 }])
 
-.controller('LeaderboardCtrl', [function() {
-
+.controller('LeaderboardCtrl', ['$scope', function($scope) {
+	$scope.players = [
+		{
+			name: 'Josh',
+			hole: 18,
+			handicap: 16,
+			scratch: 72,
+			points: 23
+		},
+		{
+			name: 'Ed',
+			hole: 18,
+			handicap: 16,
+			scratch: 72,
+			points: 23
+		},
+		{
+			name: 'Tom',
+			hole: 18,
+			handicap: 16,
+			scratch: 72,
+			points: 23
+		},
+		{
+			name: 'Adriaan',
+			hole: 18,
+			handicap: 16,
+			scratch: 72,
+			points: 23
+		}
+	]
 }]);
