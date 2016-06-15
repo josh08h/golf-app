81c543ccad80b5d669c69979093dc7dfb2737af7
Josh Hale <hale.j.0810@gmail.com>
2016-06-15 14:34:41 +0100

    input scores table

--- app/Input_Scores/input.js
+++ app/Input_Scores/input.js
@@ -3,12 +3,57 @@
 angular.module('myApp.inputScores', ['ngRoute'])
 
 	.config(['$routeProvider', function($routeProvider){
-		$routeProvider.when('/input_scores', {
+		$routeProvider.when('/input', {
 			templateUrl: 'Input_Scores/input.html',
 			controller: 'inputScoresCtrl'
 		});
 	}])
 
-	.controller('inputScoresCtrl', [function(){
+	.controller('inputScoresCtrl', ['$scope', function($scope){
+
 		
+		// TEST DATA ----------------------------------------------
+		$scope.myPlayers = [
+		{
+			name: 'Josh'
+		},
+		{
+			name: 'Tom'
+		},
+		{
+			name: 'Adriaan'
+		},
+		{
+			name: 'Ed'
+		}]
+
+
+		$scope.courseHoles = [
+		{
+			number: 1,
+			par: 4,
+			strokeIndex: 3
+		},
+		{
+			number: 2,
+			par: 3,
+			strokeIndex: 7
+		},
+		{
+			number: 3,
+			par: 4,
+			strokeIndex: 12
+		},
+		{
+			number: 4,
+			par: 5,
+			strokeIndex: 2
+		}]
+
+		// TEST DATA END ----------------------------------------------
+
+
+
+
+
 	}])
