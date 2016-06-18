b6d4a9651d3403086e70566fb5161bb19b8345c4
ThatTobMate <thale.android@gmail.com>
2016-06-18 18:27:44 +0100

    player changes

--- app/Input_Scores/input.js
+++ app/Input_Scores/input.js
@@ -11,15 +11,45 @@ angular.module('myApp.inputScores', ['ngRoute'])
 	}])
 
 	.controller('inputScoresCtrl', ['$scope', function($scope){
-		
-		var players = firebase.database().ref('players/');
-		
-		players.orderByChild('groupID')
-			.equalTo(localStorage.getItem('uid'))
-			.on('value', function(snapshot){
-				$scope.myPlayers = snapshot.val();
+		var holes = firebase.database().ref('Holes/');
+		var players = firebase.database().ref('Players/');
+		// var groups = firebase.database().ref('Groups/');
+		var scores = firebase.database().ref('Scores/');
+		$scope.myPlayers = []
+
+		holes
+			.orderByChild('CourseId')
+			.equalTo('Burstead')
+			.once('value', function(snapshot){
+				$scope.courseHoles = snapshot.val()
+				console.log(snapshot.val())
 				$scope.$digest();
+			})
+
+		players
+			.orderByChild('Groups/GroupA')
+			.equalTo(true)
+			.on('value', function(snapshot){
+				snapshot.forEach(function(cs){
+					getScores(cs)
+				})
 			});
+
+		function getScores(cs){
+			scores
+				.orderByChild('PlayerId')
+				.equalTo(cs.key)
+				.on('value', function(snap){
+					var player = cs.val()
+					player.Scores = snap.val()
+					$scope.myPlayers.push(player)
+					$scope.$digest();
+				})
+		}
+
+			// $scope.sortScores = function(player){
+			// 	debugger
+			// }
 
 	}])
 
