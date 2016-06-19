ae76e277a365080fceff9614f14a288d186c34e7
ThatTobMate <thale.android@gmail.com>
2016-06-19 01:23:44 +0100

    get players and scores

--- app/Input_Scores/input.js
+++ app/Input_Scores/input.js
@@ -13,27 +13,68 @@ angular.module('myApp.inputScores', ['ngRoute'])
 	.controller('inputScoresCtrl', ['$scope', function($scope){
 		var holes = firebase.database().ref('Holes/');
 		var players = firebase.database().ref('Players/');
+		var tournaments = firebase.database().ref('Tournaments/');
 		// var groups = firebase.database().ref('Groups/');
 		var scores = firebase.database().ref('Scores/');
 		$scope.myPlayers = []
+		$scope.addScore = false
+		$scope.score = {}
+
+		$scope.submitScores = function(){
+			console.log($scope.score)
+			var scoresLength = Object.keys($scope.score).length
+			for(var i = 0; i < scoresLength; i++){
+				var score = $scope.score[i]
+				var playerRef = players.child(score.PlayerId)
+				var tournamentRef = tournaments.child(score.TournamentId)
+				var holeRef = holes.child(score.HoleId)
+				
+				var newScore = scores.push($scope.score[i])
+				var newScoreId = newScore.key
+				debugger
+				// Update associations
+				// playerRef.child('Scores').push({
+				// 	newScoreId: true
+				// })
+				// holeRef.child('Scores').push({
+				// 	newScoreId: true
+				// })
+				tournamentRef.update({
+					"Scores":{
+						newScoreId: true
+					}
+				})
+			}
+		}
+
+		$scope.showForm = function(){
+			$scope.addScore = false
+		}
+
+		$scope.addScores = function(key, hole){
+			$scope.addScore = true
+			$scope.holeId = key
+			$scope.hole = hole
+		}
 
 		holes
 			.orderByChild('CourseId')
 			.equalTo('Burstead')
 			.once('value', function(snapshot){
 				$scope.courseHoles = snapshot.val()
-				console.log(snapshot.val())
-				$scope.$digest();
-			})
+		});
 
-		players
-			.orderByChild('Groups/GroupA')
-			.equalTo(true)
-			.on('value', function(snapshot){
-				snapshot.forEach(function(cs){
-					getScores(cs)
-				})
+
+		function refreshLeaderboard(){
+			players
+				.orderByChild('Groups/GroupA')
+				.equalTo(true)
+				.once('value', function(snapshot){
+					snapshot.forEach(function(cs){
+						getScores(cs)
+					})
 			});
+		}
 
 		function getScores(cs){
 			scores
@@ -42,15 +83,13 @@ angular.module('myApp.inputScores', ['ngRoute'])
 				.on('value', function(snap){
 					var player = cs.val()
 					player.Scores = snap.val()
+					player.PlayerId = cs.key
 					$scope.myPlayers.push(player)
 					$scope.$digest();
 				})
 		}
 
-			// $scope.sortScores = function(player){
-			// 	debugger
-			// }
-
+			refreshLeaderboard()
 	}])
 
 
