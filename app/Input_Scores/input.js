afad1a545b88a2b37ab9bb87e803a782011686cb
Josh Hale <hale.j.0810@gmail.com>
2016-06-22 23:55:38 +0100

    getting scores through data binding instead of function.

--- app/Input_Scores/input.js
+++ app/Input_Scores/input.js
@@ -9,109 +9,35 @@ angular.module('myApp.inputScores', ['ngRoute'])
 			isLogin: true
 		});
 	}])
+		
 
-	// .service('scoreService', ['$q', function($q){
-	// 	var service = {}
-
-	// 	service.holes = firebase.database().ref('Holes/');
-	// 	service.players = firebase.database().ref('players/');
-	// 	service.tournaments = firebase.database().ref('Tournaments/');
-	// 	service.scores = firebase.database().ref('Scores/');
-
-	// 	service.refreshLeaderboard = function(){
-	// 		var deferred = $q.defer();
+	.service('scoreService', ['$q', function($q){
 			
-	// 		service.players
-	// 			.orderByChild('groupID')
-	// 			.equalTo(localStorage.getItem('uid'))
-	// 			.once('once', function(snapshot){
-	// 				var myPlayers = []
+		var service = {};
 
-	// 				// snapshot.forEach(function(data){
-	// 				// 	myPlayers.push(data);
-	// 				// 	console.log('data: ', data.val())
-	// 				// });
+// -----------------------------------------------------------------
+		service.addScores = function(scoreFormData){
+				console.log('form data: ',scoreFormData);
+				for (var key in scoreFormData){
+					firebase.database().ref('Scores/').push(scoreFormData[key]);
+				};
+			};
 
-					
-	// 				// console.log('player snapshot: ', snapshot.val());
-					
-	// 				//for each player in my group, get the scores.
-	// 				snapshot.forEach(function(cs){
-	// 					console.log(cs);
-	// 					getScores(cs, myPlayers)
-	// 				});
-	// 		});
+		return service;
+		}]) // end of service
 
-		// 	function getScores(cs, myPlayers){
-		// 		var myPlayers = [];
-		// 		service.scores
-		// 			.orderByChild('PlayerId')
-		// 			.equalTo(cs.key)
-		// 			.on('once', function(snap){
-		// 				var player = cs.val()
-		// 				player.Scores = snap.val()
-		// 				player.PlayerId = cs.key
-		// 				myPlayers.push(player);
-		// 				console.log('myPlayers', myPlayers);
-		// 				//CHECK CONSOLE.LOG HERE
-		// 				// console.log('myPlayers: ', myPlayers);
-		// 				deferred.resolve(myPlayers)
-		// 			})
-		// 	}
-		// 	return deferred.promise;
-		// }
 
-	// 	service.submitScores = function(scores){
-	// 		//IF YOU CONSOLE.LOG(SCORES) THERE ARE double the amount of objects than there are players?...
-	// 		var deferred = $q.defer();
-	// 		var scoresLength = Object.keys(scores).length
-	// 		for(var i = 0; i < scoresLength; i++){
-	// 			var score = scores[i]
-	// 			var playerRef = service.players.child(score.PlayerId)
-	// 			var tournamentRef = service.tournaments.child(score.TournamentId)
-	// 			var holeRef = service.holes.child(score.HoleId)
-
-	// 			var newScore = service.scores.push(score)
-	// 			var newScoreId = newScore.key
-
-	// 			// Update associations
-	// 			playerRef.child('Scores').child(newScoreId).set(true)
-	// 			holeRef.child('Scores').child(newScoreId).set(true)
-	// 			tournamentRef.child('Scores').child(newScoreId).set(true)
-
-	// 			deferred.resolve('updated')
-	// 		}
-	// 		return deferred.promise;
-	// 	}
-
-	// 	return service
-	// }])
-
-	.controller('inputScoresCtrl', ['$scope', '$q', function($scope, $q){
+	.controller('inputScoresCtrl', ['$scope', '$q', 'scoreService', function($scope, $q, scoreService){
+		
 		var holes = firebase.database().ref('Holes/');
 		var players = firebase.database().ref('players/');
 		var tournaments = firebase.database().ref('Tournaments/');
 		var scores = firebase.database().ref('Scores/');
 		$scope.myPlayers = []
 		$scope.addScore = false
-		$scope.score = {}
+		$scope.scoreFormData = {}
 
-		// $scope.submitScores = function(){
-		// 	console.log('scope.score: ', $scope.score);
-		// 	scoreService.submitScores($scope.score).then(function(d){
-		// 		refreshLeaderboard();
-		// 	})
-		// 	$scope.score={};
-		// }
-
-		$scope.hideForm = function(){
-			$scope.addScore = false
-		}
-
-		$scope.showForm = function(){
-			$scope.addScore = true;
-		}
-
+// ------------------------------------------------------------ initialisation functions
 		var playersFnc = function(){
 			var myPlayers = [];
 			var deferred = $q.defer();
@@ -125,21 +51,6 @@ angular.module('myApp.inputScores', ['ngRoute'])
 				});
 			return deferred.promise;
 		};
-
-		// 	var scoresFunc = function($q){
-		// 	var myScores = [];
-		// 	var deferred = $q.defer();
-		// 	scores
-		// 		.orderByChild('PlayerId')
-		// 		.equalTo($scope.players)
-		// 		.once('value', function(snapshot){
-		// 			myScores.push(snapshot.val());
-		// 			deferred.resolve(myScores);
-		// 		});
-		// 	return deferred.promise;
-		// }($q).then(function(data){
-		// 	$scope.myScores = data;
-		// });
 
 
 		var holesFnc = function(){
@@ -156,6 +67,9 @@ angular.module('myApp.inputScores', ['ngRoute'])
 			return deferred.promise;
 		};
 
+//	------------------------------------------------------------ instantiating
+
+
 // Get players in my group then for each player
 // get the scores associated to them
 		playersFnc().then(function(players){
@@ -163,54 +77,31 @@ angular.module('myApp.inputScores', ['ngRoute'])
 			players.forEach(function(playersId){
 				for (var playerId in playersId){
 					scores.orderByChild('PlayerId').equalTo(playerId).on('value', function(snapshot){
-						console.log(snapshot.val());
+						console.log('scores:', snapshot.val());
 					})
 				}
 			})
 		});
 
-
 		holesFnc().then(function(holes){
 			$scope.myHoles = holes[0];
-			console.log($scope.myHoles)
 		});
 
-// get myPlayers
-// get myHoles
-// for each player in myPlayers{
-// scores.orderByChild('playerID').equalTo(player.ID).on('value', function(snapshot){
-//  
-// })
-//
-// }
 
+// --------------------------------------------------------------- html onClick functions
 
+		$scope.submitForm = function(){
+			scoreService.addScores($scope.scoreFormData);
+			$scope.hideForm();
+		};
 
+		$scope.hideForm = function(){
+			$scope.addScore = false
+		}
 
+		$scope.showForm = function(hole){
+			$scope.addScore = true;
+			$scope.hole = hole;
+		}
 
-
-
-
-		// holes;
-
-		// init($q).then(function(data){
-		// 	$scope.players = data;
-		// });
-
-		// $scope.addScores = function(key, hole){
-		// 	$scope.addScore = true
-		// 	$scope.hole = hole
-		// }
-
-		
-		// function refreshLeaderboard(){
-		// 	scoreService.refreshLeaderboard().then(function(data){
-		// 		$scope.myPlayers = data;
-		// 		$scope.hideForm()
-		// 	})
-		// }
-		// 	refreshLeaderboard();
-	}])
-
-
-
+	}]) // end of controller()
